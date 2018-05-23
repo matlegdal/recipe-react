04cbfaa29bb2a8bba80aaa5290dba1e9a50ee3b3
Mathieu Leger-Dalcourt <Mathieu.Leger-Dalcourt@fujitsu.com>
2018-05-23 16:13:44 -0400

    Recipe component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {
+  Component
+} from 'react';
 import './App.css';
+import Recipe from "./Recipe"
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App" >
+        <Recipe
+          title="pasta"
+          ingredients={["flour", "water"]}
+          instructions="Mix ingredients"
+          imgSrc="spaghetti.jpg" />
       </div>
     );
   }
 }
-
-export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,31 @@
+import React, {
+    Component
+} from "react";
+import "./Recipe.css";
+
+export default class Recipe extends Component {
+    render() {
+        const { title, instructions, imgSrc } = this.props;
+        const ingredients = this.props.ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>
+        ));
+        return (
+            <div className="recipe-card">
+                <div className="recipe-card-img">
+                    <img src={imgSrc} alt={title} />
+                </div>
+                <div className="recipe-card-content">
+                    <h2 className="recipe-card-title">Recipe {title}</h2>
+                    <h3>Ingredients:</h3>
+                    <ul>
+                        {ingredients}
+                    </ul>
+                    <h3>Instructions:</h3>
+                    <p>{instructions}</p>
+                </div>
+
+
+            </div>
+        );
+    }
+}
