f467ab9bf082c07cfb76211549d6498fb58787da
Mathieu Leger-Dalcourt <Mathieu.Leger-Dalcourt@fujitsu.com>
2018-05-23 16:52:11 -0400

    add Nav component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, {
   Component
 } from 'react';
 import './App.css';
-import RecipeList from "./RecipeList"
+import RecipeList from "./RecipeList";
+import Nav from "./Nav";
 
 export default class App extends Component {
   render() {
     return (
       <div className="App" >
+        <Nav />
         <RecipeList />
       </div>
     );
---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./Nav.css";
+
+export default class Nav extends Component {
+    render() {
+        return (
+            <header>
+                <h2><a>Recipe App</a></h2>
+                <nav>
+                    <li><a>New Recipe</a></li>
+                    <li><a>Home</a></li>
+                    <li><a>About</a></li>
+                    <li><a>Contact Us</a></li>
+                </nav>
+            </header>
+        );
+    }
+}
